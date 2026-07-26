d2d671bfe313558cbf39b96ec3538bf03e5f5061
Rolly Cavan <cavanrlee2@gmail.com>
2026-07-26 12:29:05 +0800

    adjustments in auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -57,6 +57,7 @@ class AuthController extends Controller
             return response()->json([
                 'success'       => true,
                 'message'       => 'Login successful.',
+                'auth_token'    => $sessionToken,
                 'data' => [
                     'user'                      => Auth::user(),
                     'menu'                      => Menu::get(),
