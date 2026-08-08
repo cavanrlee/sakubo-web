8d2ee2a7c5eae7dbe575e624ba618c107abe6f6f
Rolly Cavan <cavanrlee2@gmail.com>
2026-08-09 03:12:00 +0800

    build apk for testing 3

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use Illuminate\Support\Facades\File;
-
-// Lahat ng hindi API routes ay ibabato sa React index.html
-Route::get('/{any?}', function () {
-    $path = public_path('index.html');
-    
-    if (File::exists($path)) {
-        return File::get($path);
-    }
-    
-    abort(404);
-})->where('any', '.*');
+Route::get('/', function () {
+    return view('welcome');
+});
